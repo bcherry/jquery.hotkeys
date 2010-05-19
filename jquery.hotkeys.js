3541d1cda4236a58360d2152e80d31cbf01e39ae
Ben Cherry <bcherry@gmail.com>
2010-05-18 18:34:05 -0700

    added support for key sequences on any of keydown, keyup, or keypress. $(document).bind("keypress", ["a", "b", "c"], func);

--- jquery.hotkeys.js
+++ jquery.hotkeys.js
@@ -11,43 +11,54 @@
 */
 
 (function(jQuery){
-	
+
 	jQuery.hotkeys = {
 		version: "0.8",
 
 		specialKeys: {
 			8: "backspace", 9: "tab", 13: "return", 16: "shift", 17: "ctrl", 18: "alt", 19: "pause",
 			20: "capslock", 27: "esc", 32: "space", 33: "pageup", 34: "pagedown", 35: "end", 36: "home",
-			37: "left", 38: "up", 39: "right", 40: "down", 45: "insert", 46: "del", 
+			37: "left", 38: "up", 39: "right", 40: "down", 45: "insert", 46: "del",
 			96: "0", 97: "1", 98: "2", 99: "3", 100: "4", 101: "5", 102: "6", 103: "7",
-			104: "8", 105: "9", 106: "*", 107: "+", 109: "-", 110: ".", 111 : "/", 
-			112: "f1", 113: "f2", 114: "f3", 115: "f4", 116: "f5", 117: "f6", 118: "f7", 119: "f8", 
+			104: "8", 105: "9", 106: "*", 107: "+", 109: "-", 110: ".", 111 : "/",
+			112: "f1", 113: "f2", 114: "f3", 115: "f4", 116: "f5", 117: "f6", 118: "f7", 119: "f8",
 			120: "f9", 121: "f10", 122: "f11", 123: "f12", 144: "numlock", 145: "scroll", 191: "/", 224: "meta"
 		},
-	
+
 		shiftNums: {
-			"`": "~", "1": "!", "2": "@", "3": "#", "4": "$", "5": "%", "6": "^", "7": "&", 
-			"8": "*", "9": "(", "0": ")", "-": "_", "=": "+", ";": ": ", "'": "\"", ",": "<", 
+			"`": "~", "1": "!", "2": "@", "3": "#", "4": "$", "5": "%", "6": "^", "7": "&",
+			"8": "*", "9": "(", "0": ")", "-": "_", "=": "+", ";": ": ", "'": "\"", ",": "<",
 			".": ">",  "/": "?",  "\\": "|"
 		}
 	};
 
 	function keyHandler( handleObj ) {
 		// Only care when a possible input has been specified
-		if ( typeof handleObj.data !== "string" ) {
+		if ( typeof handleObj.data !== "string" && !jQuery.isArray( handleObj.data ) ) {
 			return;
 		}
-		
+
 		var origHandler = handleObj.handler,
-			keys = handleObj.data.toLowerCase().split(" ");
-	
+			isSequence = jQuery.isArray( handleObj.data ),
+			sequenceIndex = 0,
+			sequenceLength = handleObj.data.length;
+
 		handleObj.handler = function( event ) {
+			var keyData = handleObj.data,
+				keys;
+
+			if ( isSequence ) {
+				keyData = keyData[ sequenceIndex ];
+			}
+
+			keys = keyData.toLowerCase().split(" ");
+
 			// Don't fire in text-accepting inputs that we didn't directly bind to
 			if ( this !== event.target && (/textarea|select/i.test( event.target.nodeName ) ||
 				 event.target.type === "text") ) {
 				return;
 			}
-			
+
 			// Keypress represents characters, not special keys
 			var special = event.type !== "keypress" && jQuery.hotkeys.specialKeys[ event.which ],
 				character = String.fromCharCode( event.which ).toLowerCase(),
@@ -61,7 +72,7 @@
 			if ( event.ctrlKey && special !== "ctrl" ) {
 				modif += "ctrl+";
 			}
-			
+
 			// TODO: Need to make sure this works consistently across platforms
 			if ( event.metaKey && !event.ctrlKey && special !== "meta" ) {
 				modif += "meta+";
@@ -86,8 +97,29 @@
 
 			for ( var i = 0, l = keys.length; i < l; i++ ) {
 				if ( possible[ keys[i] ] ) {
-					return origHandler.apply( this, arguments );
+					if ( !isSequence || sequenceIndex === sequenceLength - 1 ) {
+						sequenceIndex = 0;
+						return origHandler.apply( this, arguments );
+					} else {
+						sequenceIndex += 1;
+						return true;
+					}
 				}
+			}
+
+			// we didn't find a match...
+			// we'll reset to 0, but check the first one for a match
+			if ( isSequence && sequenceIndex !== 0 ) {
+				keys = handleObj.data[0].toLowerCase().split(" ");
+				for ( var i = 0, l = keys.length; i < l; i++ ) {
+					if ( possible[ keys[i] ] ) {
+						sequenceIndex = 1;
+						return true;
+					}
+				}
+
+				// still no good. oh well.
+				sequenceIndex = 0;
 			}
 		};
 	}
